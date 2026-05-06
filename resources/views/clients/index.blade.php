f435242cdb42ad766b058ba32e69da05376b8376
saraabbih1 <abbihsara30@gmail.com>
2026-05-06 11:15:37 +0100

    feat: les colors du status

--- resources/views/clients/index.blade.php
+++ resources/views/clients/index.blade.php
@@ -2,10 +2,23 @@
 
 @foreach($clients as $client)
     <div style="margin-bottom:10px;">
+
         <strong>{{ $client->name }}</strong> -
         {{ $client->phone }} -
-        <span>{{ $client->status }}</span>
+
+        @if($client->status == 'new')
+            <span style="color:blue;">New</span>
+        @elseif($client->status == 'interesse')
+            <span style="color:orange;">Intéressé</span>
+        @elseif($client->status == 'paye')
+            <span style="color:green;">Payé</span>
+        @elseif($client->status == 'relance')
+            <span style="color:red;">À relancer</span>
+        @else
+            <span>{{ $client->status }}</span>
+        @endif
 
         <a href="{{ route('clients.edit', $client->id) }}">Edit</a>
+
     </div>
 @endforeach
