4c5a39f7886d709c5fead8e071e8b65e86534ea1
saraabbih1 <abbihsara30@gmail.com>
2026-05-06 10:07:12 +0100

    feat:l'ajoute de clientcontroller

--- app/Http/Controllers/ClientController.php
+++ app/Http/Controllers/ClientController.php
@@ -1,10 +1,23 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Client;
 use Illuminate\Http\Request;
 
 class ClientController extends Controller
 {
-    //
+    //affichage du clients 
+    public function index(){
+        $client=Client::all();
+        return view('clients.index',compact('clients'));
+    }
+//from ajouter
+public function  create(){
+    return view('clients.create');
+}
+//stocage des client
+public function store(Request $request){
+    Client::create($request->all());
+    return redirect()->route('clients.index');
+}
 }
