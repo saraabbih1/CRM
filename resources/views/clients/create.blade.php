a699baa1c94f3c41021d40e06178d39bda92349a
saraabbih1 <abbihsara30@gmail.com>
2026-05-06 11:05:51 +0100

    feat:status

--- resources/views/clients/create.blade.php
+++ resources/views/clients/create.blade.php
@@ -2,5 +2,13 @@
     @csrf
     <input type="text" name="name" placeholder="Nom">
     <input type="text" name="phone" placeholder="Téléphone">
+
+     <select name="status">
+        <option value="new">New</option>
+        <option value="interesse">Intéressé</option>
+        <option value="paye">Payé</option>
+        <option value="relance">À relancer</option>
+    </select>
+    
     <button type="submit">Ajouter</button>
 </form>
